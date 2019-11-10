4eedfc5747a9023096511f868e1230993657ffaf
Dylan Kreth <dylankreth@gmail.com>
2019-11-10 11:37:09 -0600

    implement popup

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -13,5 +13,13 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.message === "run_analysis") {
     console.log("From background.js:\n", request.content);
+    var rating = request.number;
+    if ((rating = -1)) {
+      chrome.browserAction.setPopup({ popup: "liberal.html" });
+    } else if ((rating = 1)) {
+      chrome.browserAction.setPopup({ popup: "conservative.html" });
+    } else {
+      chrome.browserAction.setPopup({ popup: "neutral.html" });
+    }
   }
 });
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -5,7 +5,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log("received request");
     // console.log(theText);
 
+    //generate a number
+    var theNum = Math.round(Math.random() * 2 - 1);
     //send theText to background.js
-    chrome.runtime.sendMessage({ message: "run_analysis", content: theText });
+    chrome.runtime.sendMessage({
+      message: "run_analysis",
+      content: theText,
+      number: theNum
+    });
   }
 });
